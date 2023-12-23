96ff30b11d4b9686de661b99447b50ecf9c63192
Sandi Yusuf <snder12nr@gmail.com>
2023-12-23 23:23:40 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,6 +41,9 @@ export function Home() {
                 <Button variant="nowarthuran" className="my-4 text-base">
                   <Link to={`/post/${post["id"]}`}>Read More</Link>
                 </Button>
+                <Button variant="nowarthuran" className="my-4 text-base">
+                  <Link to={`/post/${post["id"]}`}>Read More</Link>
+                </Button>
               </div>
             </div>
           </div>
